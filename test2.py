8d54649d43c02593dd71ea3bbbb49c0f89cd4834
Dennis Evangelista <devangelista@mbs.net>
2022-02-09 18:46:48 -0500

    second test finding desired words in english

---
+++ test2.py
@@ -0,0 +1,20 @@
+"""
+test2.py
+D Evangelista, 2022
+Find all words in the English language that fit a pattern
+Requires loading words.txt from https://github.com/dwyl/english-words
+"""
+
+import re # module with regular expressions
+
+
+if __name__ == "__main__":
+
+    re_object = re.compile(r"\b[a-j]{10}\b") # this line makes the search object
+
+    for line in open("words.txt","r"):
+        for match in re_object.finditer(line):
+                print(match.group(0))
+
+    
+    
